d1cb5e4561442f126dd1958940c9c2ac58eb0365
Harshal Parekh <98951014+harshalparekh@users.noreply.github.com>
2022-08-06 19:15:39 +0530

    Add files via upload

---
+++ project1/script.js
@@ -0,0 +1,159 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+       autoplay:true,
+      slidesToShow:3,
+      slidesToScroll:1,
+      prevArrow:".prev-btn",
+      nextArrow:".next-btn",
+      responsive:[
+         {
+            breakpoint:992,
+            settings:{
+             slidesToShow:2,
+            }
+         },
+         {
+          breakpoint:768,
+          settings:{
+           slidesToShow:1,
+          }
+       }
+      ]
+ 
+    });
+ 
+    $('.nav-trigger').click(function(){
+       $('.site-content-wrapper').toggleClass('scaled');
+    })
+ });
+
+
+// Countdown Timer :
+
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "08/9/",
+        Deals = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > Deals) {
+      Deals = dayMonth + nextYear;
+    }
+    //end
+    
+    const countDown = new Date(Deals).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "Heavy Deals on Your Favourite Dishes !";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
+
+// active navbar :
+
+  const activePage = window.location.pathname;
+  const navLinks = document.querySelectorAll('nav a').forEach(link => {
+    if (link.href.includes(`${activePage}`)) {
+    link.classList.add('active');
+    }
+})
+
+// gallery ----------------
+
+var currentSlide = 0;
+const slides = document.querySelectorAll(".slide")
+const dots = document.querySelectorAll('.dot')
+
+const init = (n) => {
+  slides.forEach((slide) => {
+    slide.style.display = "none"
+    dots.forEach((dot) => {
+      dot.classList.remove("active")
+    })
+  })
+  slides[n].style.display = "block"
+  dots[n].classList.add("active")
+}
+document.addEventListener("DOMContentLoaded", init(currentSlide))
+const next = () => {
+    currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
+    init(currentSlide)
+  }
+  
+  const prev = () => {
+    currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
+    init(currentSlide)
+  }
+  
+  document.querySelector(".next").addEventListener('click', next)
+  document.querySelector(".prev").addEventListener('click', prev)
+//   ss :- 
+
+setInterval(() => {
+    next()
+  }, 5000);
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      init(index)
+      currentSlide = i
+    })
+  })
+
+//shop
+
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-green";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
+
+// try active sidebar
+// Add active class to the current button (highlight it)
+// var header = document.getElementById("activesb");
+// var btns = header.getElementById("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//   var current = document.getElementById("active");
+//   if (current.length > 0) { 
+//     current[0].Id = current[0].Id.replace(" active ", "");
+//   }
+//   this.Id += " active ";
+//   });
+// }
+
+const activesb = window.location.pathname;
+  const navLink = document.querySelectorAll('sidebar').forEach(link => {
+    if (link.href.includes(`${btn}`)) {
+    link.classList.add('active');
+    }
+})
